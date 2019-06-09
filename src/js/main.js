4608e13d8f04bc3c69e32f07e3b3a11463266750
Adrian Praja <adrian@databank.ro>
2019-06-09 12:39:14 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-
 Ractive.components.slider = Ractive.extend({
 	isolated: true,
 	template: `
 	<div class="dbk-slider {{class}}" style="{{style}}">
-		<input type="range" min="{{min}}" max="{{max}}" value="{{value}}" step="{{step}}" id="{{id}}">
+		<input type="range" min="{{min}}" max="{{max}}" value="{{value}}" step="{{step}}" id="{{id}}" {{#if disabled}}disabled{{/if}}>
 	</div>
 	`
 });
@@ -11,8 +10,8 @@ Ractive.components.range = Ractive.extend({
 	isolated: true,
 	template: `
 	<div class="dbk-slider {{class}}" style="{{style}}">
-		<input type="range" min="{{min}}" max="{{max}}" value="{{value_from}}"  step="{{step}}" id="{{id}}">
-		<input type="range" min="{{min}}" max="{{max}}" value="{{value_to}}"    step="{{step}}" id="{{id}}">
+		<input type="range" min="{{min}}" max="{{max}}" value="{{value_from}}"  step="{{step}}" id="{{id}}" {{#if disabled}}disabled{{/if}}>
+		<input type="range" min="{{min}}" max="{{max}}" value="{{value_to}}"    step="{{step}}" id="{{id}}" {{#if disabled}}disabled{{/if}}>
 	</div>
 	`
 });
